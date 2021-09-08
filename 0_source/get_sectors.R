ff1835a531f92541aaf4556f56a9b979b7ba9d83
Cesar Jimenez <cesar.2591@gmail.com>
2021-09-08 15:56:33 -0400

    Translating variables' names. Removing "All neighborhoods" from the list of neighborhoods. Commenting the script. Looping from province = 1:50, instead of 1:3.

--- 0_source/get_sectors.R
+++ 0_source/get_sectors.R
@@ -1,28 +1,55 @@
+# Libraries
 library(dplyr)
 library(readr)
 library(rvest)
 library(stringi)
 
-df <- data.frame(id = character(),
+# Creating empty data frame
+df <- data.frame(province = character(),
+                 id = character(),
                  descripcion = character())
-for (ciudad in 1:3) {
-    lista <- paste0("https://www.supercasas.com/assets/js/autocomplete-search-location-sectors.js?text=&val=&province=",
-                    ciudad)
-    lista_html <- read_html(lista)
+
+# There are 31 provinces in the Dominican Republic, but supercasas.com considers
+# municipalities as provinces, depending on its importance. The province ID does
+# not increases orderly (some numerals have no province assigned to it).
+for (province in 1:50) {
+    # URL to iterate looking for all neighborhoods in each province
+    base_url <- "https://www.supercasas.com/assets/js/autocomplete-search-location-sectors.js?text=&val=&province="
+    list_url <- paste0(base_url,
+                    province)
+    
+    # Extracting HTML
+    list_html <- read_html(list_url)
+    
+    # Verifying whether the URL contains a list of neighborhoods, the loop will
+    # continue only if it contains a list
+    empty_list <- stri_detect_fixed(html_text(list_html), "Selecciona")
+    if(empty_list){
+        next
+    }
+    
     sector <- 1
+    
     repeat{
-        links <- html_nodes(lista_html, paste0("body > div > ul > li:nth-child(",
-                                               sector,
-                                               ")"))
+        links <- html_nodes(list_html,
+                            paste0("body > div > ul > li:nth-child(",
+                                   sector,
+                                   ")"))
         len <- length(links)
         if(len == 0){
             break
         } else {
-            df <- rbind(df, data.frame(ciudad = ciudad,
+            df <- rbind(df, data.frame(province = province,
                                        id = html_attrs(links)[[1]][3],
-                                       descripcion = html_attrs(links)[[1]][4]))
+                                       description = html_attrs(links)[[1]][4]))
             sector = sector + 1
         }
     }
 }
-  
+
+# Removing the option "All neighborhoods" from the data set
+df <- df %>%
+    filter(description != "Todos los sectores") %>%
+    distinct(id, .keep_all = TRUE)
+
+write.csv("./1_data/0_raw/neighborhoods.csv")
